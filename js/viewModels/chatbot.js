4e5de9a2686ab48a46636b3dae2c744f531f3f84
nataliat <nataliat@NataliaW7.advahoo.local>
2017-05-25 17:49:16 +0300

    calling RestApp query and returning chatHistory

--- js/viewModels/chatbot.js
+++ js/viewModels/chatbot.js
@@ -45,9 +45,50 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojselectcombo
                 else
                     jsId = jsId.substring(11);
             }
+            console.log("SESSION ID!!!!!!!!");
+            console.log(jsId);
             return jsId;
         }
 
+        $(document).ready(function () {
+
+            var queryResponse;
+            $.ajax({
+                url: 'http://localhost:8080/RestTest/resources/com.airhacks.chatline/userId/2',
+                method: 'GET',
+                dataType: "xml",
+                crossDomain: true,
+                success: function (responseXML) {
+                    var xmlFromQuery = responseXML;
+
+                    var txt = "";
+                    var x = responseXML.getElementsByTagName('chatLine');
+                    console.log(x);
+                    for (i = 0; i < x.length; i++) {
+                        //txt += x[i].find('chatLine').find('botResponse').text();
+
+                        var Robotxt = x[i].firstChild.innerHTML;
+                        var Usertxt = x[i].getElementsByTagName('lineText')[0].textContent;
+                        console.log(Robotxt);
+                        $(".chat").append('<li class="self"><div class="avatar"></div><div class="msg"><p>' + Usertxt + '</p></div></li>');
+                        $(".chat").append('<li class="other"><div class="avatar"></div><div class="msg"><p>' + Robotxt + '</p></div></li>');
+                    }
+
+                    console.log("-------------------");
+                    var chatLine = $(responseXML).find("chatLines").find('chatLine').find('botResponse').text();
+                    console.log(chatLine);
+
+
+
+                    console.log("APEL QUERY DB --------------");
+                    console.log(responseXML);
+                    queryResponse = responseXML;
+                    console.log(queryResponse.getElementsByTagName('chatLines')[0].firstChild.nodeValue);
+                    
+                }
+            });
+
+        })
 
         //ON PRESSING THE "ENTER" KEY
         $(document).keypress(function (e) {
@@ -56,7 +97,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojselectcombo
             if (e.keyCode === 13) {
                 $('#userfield').val('');
             }
-            
+
 
             if (e.which === 13) {
 //                alert("BAM");
@@ -81,7 +122,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojselectcombo
                     url: 'http://127.0.0.1:5000/converse',
                     data: {
                         'q': self.userMsg(),
-                        'session_id': getJSessionId()
+                        'session_id': '1243'
                     },
                     method: 'GET',
                     crossDomain: true,
